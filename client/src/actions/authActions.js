b4b9fd2a5aa2e749d364d2c96c00d6396162b7e6
Himon Rana <himon.rana@chasacademy.se>
2018-07-23 16:39:32 +0200

    fixed changes

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,7 +5,10 @@ import { GET_ERRORS } from "./types";
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/auth/register", userData)
-    .then(res => history.push("/"))
+    .then(res => {
+      history.push("/");
+      console.log('You are registered now!');
+    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
