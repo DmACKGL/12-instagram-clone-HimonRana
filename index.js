e408a2e9bfb4c0cfefe219d15646eb6908886d0b
Himon Rana <himon.rana@chasacademy.se>
2018-08-20 14:23:06 +0200

    prepare for deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const passport = require("passport");
+const path = require("path");
 
 const UserController = require("./controllers/UserController");
 const AuthController = require("./controllers/AuthController");
@@ -19,5 +20,15 @@ require("./config/passport")(passport);
 app.use("/auth", AuthController);
 app.use("/profile", ProfileController);
 app.use("/posts", PostController);
+
+// Server static assets if in product
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
