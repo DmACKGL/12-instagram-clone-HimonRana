e5c5ca98fe0b021f468b0795d6ca344949ea189e
Franco Sanllehi <fsanllehi@gamerlive.cl>
2018-08-20 09:31:04 -0300

    Remove MongoDB string with credentials

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose
-    .connect('mongodb://admin:admin@ds259258.mlab.com:59258/buntstagram')
+    .connect(process.env.MDB)
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.log(err));
